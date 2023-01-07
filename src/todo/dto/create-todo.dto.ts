11471e8be549c4fcd6fcf4753e148bdc7a832f69
Ali Nategh <alinategh005@gmail.com>
2023-01-07 17:39:32 +0330

    feat: impl request auth

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,16 +1,19 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsString, IsOptional } from "class-validator";
 
 export class CreateTodoDto {
     @IsString()
     @IsNotEmpty()
     readonly title: string;
 
+    @IsOptional()
     @IsString()
     readonly description?: string;
 
+    @IsOptional()
     @IsString()
     readonly color?: string;
 
+    @IsOptional()
     @IsString({ each: true })
     readonly tags?: string[];
 }
--- src/todo/dto/update-todo.dto.ts
+++ src/todo/dto/update-todo.dto.ts
@@ -1,15 +1,19 @@
-import { IsString } from "class-validator";
+import { IsString, IsOptional } from "class-validator";
 
 export class UpdateTodoDto {
+    @IsOptional()
     @IsString()
     readonly title?: string;
 
+    @IsOptional()
     @IsString()
     readonly description?: string;
 
+    @IsOptional()
     @IsString()
     readonly color?: string;
 
+    @IsOptional()
     @IsString({ each: true })
     readonly tags?: string[];
 }
