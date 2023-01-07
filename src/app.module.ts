74d338738bd99f5d317f54030912ad2bbde862b7
Ali Nategh <alinategh005@gmail.com>
2023-01-07 19:50:58 +0330

    feat: adding TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { TodoController } from './todo/todo.controller';
 import { TodoService } from './todo/todo.service';
 import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nest',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tag } from './entities/tag.entity';
+import { Todo } from './entities/todo.entity';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([Todo, Tag])],
     controllers: [TodoController],
     providers: [TodoService]
 })
