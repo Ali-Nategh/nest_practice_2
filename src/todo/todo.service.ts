54ff5307f5ac073342a10e01a11e4813c571959b
Ali Nategh <alinategh005@gmail.com>
2023-01-08 09:46:31 +0330

    feat: add tags relation

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -17,36 +17,47 @@ export class TodoService {
     ) { }
 
     findAll() {
-        return this.todoRepository.find({
-            relations: ['tags'],
-        });
+        return this.todoRepository.find({ relations: ['tags'] });
     }
 
     async findOne(id: string) {
-        const todo = await this.todoRepository.findOne({ where: { id: +id }, relations: ['tags'], });
+        const todo = await this.todoRepository.findOne({ where: { id: +id }, relations: ['tags'] });
         // if (!todo) throw new HttpException(`todo #${id} not found`, HttpStatus.NOT_FOUND)
         if (!todo) throw new NotFoundException(`todo #${id} not found`);
         return todo
     }
 
     async create(createTodoDto: CreateTodoDto) {
-        const tags = await Promise.all(createTodoDto.tags.map(name => this.preloadTagByName(name)));
+        let tags = createTodoDto.tags;
+
+        let tagsChanged: Tag[] = [];
+        if (tags) {
+            for (let i = 0; i < tags.length; i++) {
+                tagsChanged.push(await this.preloadTagByName(tags[i]))
+            }
+        }
 
         const todo = this.todoRepository.create({
             ...createTodoDto,
-            tags,
-        }
-        );
+            tags: [...tagsChanged],
+        });
         return this.todoRepository.save(todo);
     }
 
     async update(id: string, updateTodoDto: UpdateTodoDto) {
-        const tags = updateTodoDto.tags && await Promise.all(updateTodoDto.tags.map(name => this.preloadTagByName(name)));
+        let tags = updateTodoDto.tags;
+
+        let tagsChanged: Tag[] = [];
+        if (tags) {
+            for (let i = 0; i < tags.length; i++) {
+                tagsChanged.push(await this.preloadTagByName(tags[i]))
+            }
+        }
 
         const todo = await this.todoRepository.preload({
             id: +id,
             ...updateTodoDto,
-            tags,
+            tags: [...tagsChanged],
         });
         if (!todo) throw new NotFoundException(`Todo #${id} not found`);
         return this.todoRepository.save(todo);
@@ -58,7 +69,7 @@ export class TodoService {
     }
 
     private async preloadTagByName(name: string): Promise<Tag> {
-        const existingTag = await this.tagRepository.findOne({ where: { name } });
+        const existingTag = await this.tagRepository.findOne({ where: { name, } });
         if (existingTag) return existingTag;
         return this.tagRepository.create({ name });
     }
